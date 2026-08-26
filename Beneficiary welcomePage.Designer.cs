85a9856f09c1bf7ba0cd7006eb1f87aac729847a
Dimphow <didieyy46@gmail.com>
2026-08-26 17:37:50 +0200

    Initial empty commit for branch

---
+++ Beneficiary welcomePage.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace Sassa_Queue_And_Service_Management_System
+{
+    partial class Beneficiary_welcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Beneficiary_welcomePage";
+        }
+
+        #endregion
+    }
+}
