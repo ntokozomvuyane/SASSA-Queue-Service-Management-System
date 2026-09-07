14122f8fea37d4749be6127a068ad57059483272
God's_favourite <Rita Amoako@DESKTOP-PK3SPKK>
2026-09-07 12:41:35 +0200

    save program startup changes

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Sassa_Queue_And_Service_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyProfileForm());
+            Application.Run(new QueueStatusForm());
         }
     }
 }
