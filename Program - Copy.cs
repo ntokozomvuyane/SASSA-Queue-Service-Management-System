a3dfc2dbc1e08da535530cb29b4035479690ab3c
Dimphow <didieyy46@gmail.com>
2026-09-07 21:21:19 +0200

    Completed beneficiary page

---
+++ Program - Copy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sassa_Queue_And_Service_Management_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmSassaQueueAndServiceManagementSystem());
+            Application.Run(new frmBeneficiary_welcomePage());
+           //Application.Run(new LoginForm());
+        }
+    }
+}
---
+++ RegistrationForm.Designer - Copy.cs
@@ -0,0 +1,325 @@
+﻿namespace Sassa_Queue_And_Service_Management_System
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblBeneficiaryPortal = new System.Windows.Forms.Label();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.lblBeneficiaryRegistration = new System.Windows.Forms.Label();
+            this.lblSassa = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pnlRegistration = new System.Windows.Forms.Panel();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtBeneficiaryID = new System.Windows.Forms.TextBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.lblCreateAccount = new System.Windows.Forms.Label();
+            this.pnlHeader.SuspendLayout();
+            this.pnlRegistration.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(74)))), ((int)(((byte)(122)))));
+            this.pnlHeader.Controls.Add(this.lblBeneficiaryPortal);
+            this.pnlHeader.Controls.Add(this.lblLogo);
+            this.pnlHeader.Controls.Add(this.lblBeneficiaryRegistration);
+            this.pnlHeader.Controls.Add(this.lblSassa);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1021, 84);
+            this.pnlHeader.TabIndex = 7;
+            // 
+            // lblBeneficiaryPortal
+            // 
+            this.lblBeneficiaryPortal.AutoSize = true;
+            this.lblBeneficiaryPortal.ForeColor = System.Drawing.Color.White;
+            this.lblBeneficiaryPortal.Location = new System.Drawing.Point(859, 44);
+            this.lblBeneficiaryPortal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBeneficiaryPortal.Name = "lblBeneficiaryPortal";
+            this.lblBeneficiaryPortal.Size = new System.Drawing.Size(141, 18);
+            this.lblBeneficiaryPortal.TabIndex = 3;
+            this.lblBeneficiaryPortal.Text = "Beneficiary Portal";
+            this.lblBeneficiaryPortal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.ForeColor = System.Drawing.Color.White;
+            this.lblLogo.Location = new System.Drawing.Point(61, 44);
+            this.lblLogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(283, 16);
+            this.lblLogo.TabIndex = 2;
+            this.lblLogo.Text = "Secure access to your SASSA bookings";
+            // 
+            // lblBeneficiaryRegistration
+            // 
+            this.lblBeneficiaryRegistration.AutoSize = true;
+            this.lblBeneficiaryRegistration.ForeColor = System.Drawing.Color.White;
+            this.lblBeneficiaryRegistration.Location = new System.Drawing.Point(112, 15);
+            this.lblBeneficiaryRegistration.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBeneficiaryRegistration.Name = "lblBeneficiaryRegistration";
+            this.lblBeneficiaryRegistration.Size = new System.Drawing.Size(187, 18);
+            this.lblBeneficiaryRegistration.TabIndex = 1;
+            this.lblBeneficiaryRegistration.Text = "Beneficiary Registration";
+            // 
+            // lblSassa
+            // 
+            this.lblSassa.AutoSize = true;
+            this.lblSassa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSassa.ForeColor = System.Drawing.Color.White;
+            this.lblSassa.Location = new System.Drawing.Point(15, 15);
+            this.lblSassa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSassa.Name = "lblSassa";
+            this.lblSassa.Size = new System.Drawing.Size(75, 22);
+            this.lblSassa.TabIndex = 0;
+            this.lblSassa.Text = "SASSA";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(74)))), ((int)(((byte)(122)))));
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(30, 760);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(211, 56);
+            this.btnBack.TabIndex = 11;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pnlRegistration
+            // 
+            this.pnlRegistration.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlRegistration.Controls.Add(this.txtConfirmPassword);
+            this.pnlRegistration.Controls.Add(this.btnRegister);
+            this.pnlRegistration.Controls.Add(this.txtPassword);
+            this.pnlRegistration.Controls.Add(this.txtEmail);
+            this.pnlRegistration.Controls.Add(this.txtBeneficiaryID);
+            this.pnlRegistration.Controls.Add(this.lblFullName);
+            this.pnlRegistration.Controls.Add(this.lblID);
+            this.pnlRegistration.Controls.Add(this.lblConfirmPassword);
+            this.pnlRegistration.Controls.Add(this.lblEmail);
+            this.pnlRegistration.Controls.Add(this.lblPassword);
+            this.pnlRegistration.Controls.Add(this.txtFullName);
+            this.pnlRegistration.Controls.Add(this.lblCreateAccount);
+            this.pnlRegistration.Location = new System.Drawing.Point(111, 108);
+            this.pnlRegistration.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pnlRegistration.Name = "pnlRegistration";
+            this.pnlRegistration.Size = new System.Drawing.Size(849, 613);
+            this.pnlRegistration.TabIndex = 15;
+            this.pnlRegistration.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(174, 450);
+            this.txtConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.PasswordChar = '*';
+            this.txtConfirmPassword.Size = new System.Drawing.Size(274, 24);
+            this.txtConfirmPassword.TabIndex = 11;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(74)))), ((int)(((byte)(122)))));
+            this.btnRegister.FlatAppearance.BorderSize = 0;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(158, 521);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(380, 56);
+            this.btnRegister.TabIndex = 12;
+            this.btnRegister.Text = "Create Account";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(174, 358);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(274, 24);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(174, 290);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(274, 24);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtBeneficiaryID
+            // 
+            this.txtBeneficiaryID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBeneficiaryID.Location = new System.Drawing.Point(174, 211);
+            this.txtBeneficiaryID.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtBeneficiaryID.Name = "txtBeneficiaryID";
+            this.txtBeneficiaryID.Size = new System.Drawing.Size(274, 24);
+            this.txtBeneficiaryID.TabIndex = 8;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName.Location = new System.Drawing.Point(146, 107);
+            this.lblFullName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(84, 18);
+            this.lblFullName.TabIndex = 7;
+            this.lblFullName.Text = "Full Name";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(146, 190);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(112, 18);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "Beneficiary ID";
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.Location = new System.Drawing.Point(155, 419);
+            this.lblConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(148, 18);
+            this.lblConfirmPassword.TabIndex = 5;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(146, 269);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(50, 18);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(146, 337);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 18);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFullName.Location = new System.Drawing.Point(174, 137);
+            this.txtFullName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(274, 24);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // lblCreateAccount
+            // 
+            this.lblCreateAccount.AutoSize = true;
+            this.lblCreateAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreateAccount.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblCreateAccount.Location = new System.Drawing.Point(70, 33);
+            this.lblCreateAccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCreateAccount.Name = "lblCreateAccount";
+            this.lblCreateAccount.Size = new System.Drawing.Size(278, 36);
+            this.lblCreateAccount.TabIndex = 0;
+            this.lblCreateAccount.Text = "Create an Account";
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 828);
+            this.Controls.Add(this.pnlRegistration);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.btnBack);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "RegistrationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegistrationForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlRegistration.ResumeLayout(false);
+            this.pnlRegistration.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblBeneficiaryPortal;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.Label lblBeneficiaryRegistration;
+        private System.Windows.Forms.Label lblSassa;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel pnlRegistration;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtBeneficiaryID;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Label lblCreateAccount;
+    }
+}
