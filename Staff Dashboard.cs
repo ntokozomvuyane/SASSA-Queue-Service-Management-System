6954b8db4eaa3c48c2b0fe0d53e1005ab64937fe
Esther Mukendi <mukendie568@gmail.com>
2026-09-15 06:57:24 -0700

    Design Search Booking

---
+++ Staff Dashboard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SASSAQueueManagementSystem
+{
+    public partial class StaffQueueForm : Form
+    {
+        public StaffQueueForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvQueue_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void SASSA_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
